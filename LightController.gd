76af9bc6bc0402b09ca5bae4002be4a35b02c306
ostromann <oliver.stromann@gmail.com>
2024-10-30 23:20:26 +0100

    feat: send on and off commands. Implemented a colorqueue, such that the last set color will always appear after cooldowns.

--- LightController.gd
+++ LightController.gd
@@ -3,6 +3,7 @@ extends Node
 
 
 var ncat_path = "C:/Program Files (x86)/Nmap/ncat.exe"
+var queuedColor
 
 @onready var AUTO_KILL_PROCESS = preload("res://AutoKillProcess.tscn")
 @onready var BULB = preload("res://Bulb.tscn")
@@ -17,55 +18,42 @@ func _process(delta: float):
 	for process in $Processes.get_children():
 		$PidList.add_item(str(process.pid))
 
+func create_auto_kill_process(command, arguments):
+	var pid = OS.create_process(command, arguments)
+	var process = AUTO_KILL_PROCESS.instantiate()
+	process.pid = pid
+	$Processes.add_child(process)
 
-func _on_turn_on_button_pressed() -> void:
-	set_light_rgb()
+func set_light_off(ip: String = "192.168.0.47"):
+	create_auto_kill_process('send_light_off_command.bat', [ip])
 	
-func send_rgb_command():
-	pass
+func set_light_on(ip: String = "192.168.0.47"):
+	create_auto_kill_process('send_light_on_command.bat', [ip])
 
-func set_light_rgb(ip: String = "192.168.0.47", red: int = 255, green: int = 255, blue: int = 255, dimming: int = 100) -> void:
-	dimming = clamp(dimming, 11, 100)
-	red = clamp(red, 0, 255)
-	green = clamp(green, 0, 255)
-	blue = clamp(blue, 0, 255)
-	
-	
-	# Create the JSON command for the lights
-	var json_command = '{"id":1,"method":"setPilot","params":{"r":%d,"g":%d,"b":%d,"dimming":%d}}' % [red, green, blue, dimming]
-
-	
-	var ncat_path = "C:\\Program Files (x86)\\Nmap\\ncat.exe"
-	var command = 'echo %s | "%s" -u -w 1 %s 38899' % [json_command, ncat_path, ip]
-	#print(command)
-	
-	var exit_code = OS.execute("cmd", ["/c", command])
-
-	if exit_code == OK:
-		print("Command sent successfully!")
-	else:
-		print("Failed to send command, exit code:", exit_code)
-
-func set_light_rgb_bat(ip: String = "192.168.0.47", red: int = 255, green: int = 255, blue: int = 255, dimming: int = 75) -> void:
+func set_light_rgb(ip: String = "192.168.0.47", red: int = 255, green: int = 255, blue: int = 255, dimming: int = 75) -> void:
 	dimming = clamp(dimming, 11, 99)
 	red = clamp(red, 0, 255)
 	green = clamp(green, 0, 255)
 	blue = clamp(blue, 0, 255)
 	
-	var command = 'send_light_command.bat %d %d %d %d %s' % [red, green, blue, dimming, ip]
-	var pid = OS.create_process('send_light_command.bat', [red, green, blue, dimming, ip])
-	var process = AUTO_KILL_PROCESS.instantiate()
-	process.pid = pid
-	$Processes.add_child(process)
-	
-	
-	
+	create_auto_kill_process('send_light_command.bat', [red, green, blue, dimming, ip])
 	$ColorUpdateCooldown.start()
 
-	print("created process ", pid)
+func set_color(color):
+	$ColorRect.color = color
+	set_light_rgb("192.168.0.47", int(color.r*255), int(color.g*255), int(color.b*255), 75)
+	queuedColor = null
+	
+func _on_turn_on_button_pressed() -> void:
+	set_light_on()
+	
+func _on_turn_off_button_pressed() -> void:
+	set_light_off()
 
+func _on_color_picker_button_color_changed(color: Color) -> void:	
+	queuedColor = color
 
-func _on_color_picker_button_color_changed(color: Color) -> void:
-	if $ColorUpdateCooldown.is_stopped():
-		$ColorRect.color = color
-		set_light_rgb_bat("192.168.0.47", int(color.r*255), int(color.g*255), int(color.b*255), 75)
+func _on_color_update_cooldown_timeout() -> void:
+	if queuedColor:
+		set_color(queuedColor)
+		
--- send_light_on_command.bat
+++ send_light_on_command.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
-set json_command={"id":1,"method":"setPilot","params":{"r":%1,"g":%2,"b":%3,"dimming":%4}}
-echo %json_command% | "C:\\Program Files (x86)\\Nmap\\ncat.exe" -u -w 1 %5 38899
+set json_command={"id":1,"method":"setState","params":{"state":true}}
+echo %json_command% | "C:\\Program Files (x86)\\Nmap\\ncat.exe" -u -w 1 %1 38899
 endlocal
